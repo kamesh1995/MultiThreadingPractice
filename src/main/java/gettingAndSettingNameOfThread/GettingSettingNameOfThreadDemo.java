754b3454713b5a13b4913b9cd29b8a04f3e67ef8
Kamesh <kamesh11nayak@gmail.com>
2026-01-04 23:51:31 +0530

    demo of getting and setting name of thread

---
+++ src/main/java/gettingAndSettingNameOfThread/GettingSettingNameOfThreadDemo.java
@@ -0,0 +1,14 @@
+package gettingAndSettingNameOfThread;
+
+class MyThreadTwo extends Thread{
+
+}
+public class GettingSettingNameOfThreadDemo {
+    public static void main(String[] args) {
+        MyThreadTwo t = new MyThreadTwo();
+        System.out.println(t.getName());
+        System.out.println(Thread.currentThread().getName());
+        Thread.currentThread().setName("Customised Name for main Thread");
+        System.out.println("main Thread name is set to: "+Thread.currentThread().getName());
+    }
+}

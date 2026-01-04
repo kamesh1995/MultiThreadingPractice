4bc9cd0dab59c7a903ed3da170c0d53a268eee17
Kamesh <kamesh11nayak@gmail.com>
2026-01-04 12:00:15 +0530

    defining thread by extedning Thread class

--- src/main/java/definingthread/MyThread.java
+++ src/main/java/definingthread/MyThread.java
@@ -1,4 +1,8 @@
 package definingthread;
 
-public class MyThread {
+public class MyThread extends Thread{
+    public void run(){
+       for(int i=0;i<10;i++)
+           System.out.println("child thread");
+    }
 }
---
+++ src/main/java/definingthread/ThreadDemo.java
@@ -0,0 +1,10 @@
+package definingthread;
+
+public class ThreadDemo {
+    public static void main(String[] args) {
+        MyThread myThread=new MyThread();
+        myThread.start();
+        for(int i=0;i<10;i++)
+            System.out.println("main thread");
+    }
+}

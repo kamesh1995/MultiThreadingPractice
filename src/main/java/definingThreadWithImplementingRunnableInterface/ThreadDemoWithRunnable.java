26320a02956072494eaec801936bac16973bc92a
Kamesh <kamesh11nayak@gmail.com>
2026-01-04 12:17:00 +0530

    defining thread by implemnting Runnable interface

---
+++ src/main/java/definingThreadWithImplementingRunnableInterface/ThreadDemoWithRunnable.java
@@ -0,0 +1,11 @@
+package definingThreadWithImplementingRunnableInterface;
+
+public class ThreadDemoWithRunnable {
+    public static void main(String[] args) {
+        MyRunnable r = new MyRunnable();
+        Thread t = new Thread(r);
+        t.start();
+        for(int i = 0; i<10; i++)
+          System.out.println("main thread");
+    }
+}

f344540ef58c92f70d371ec19f4c84dc4edac1f3
Kamesh <kamesh11nayak@gmail.com>
2026-01-04 12:07:09 +0530

    if we are not overriding run method

---
+++ src/main/java/definingthread/MyThreadDemoThree.java
@@ -0,0 +1,9 @@
+package definingthread;
+
+public class MyThreadDemoThree {
+    public static void main(String[] args) {
+        MyThreadWithNoRunMethod t = new MyThreadWithNoRunMethod();
+        t.start();
+        System.out.println("main thread");
+    }
+}
